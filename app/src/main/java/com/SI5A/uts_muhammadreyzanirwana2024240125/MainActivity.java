7af2765eb87fd37d8aacfc90a283ea5393d063af
MuhammadReyzaNirwana <dryzanetwork@mhs.mdp.ac.id>
2022-10-31 14:05:55 +0700

    comment

---
+++ app/src/main/java/com/SI5A/uts_muhammadreyzanirwana2024240125/MainActivity.java
@@ -0,0 +1,14 @@
+package com.SI5A.uts_muhammadreyzanirwana2024240125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
